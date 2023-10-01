38bf92ddf71c64480f3dc616523537eb73e3e223
andreappmarketing <andre@appmarketing.com.br>
2023-10-01 11:00:55 -0300

    fix(tests): Corrige testes de cadastrarCliente, que não estavam funcionando

--- src/infra/database/mongodb/adapters/MongoConnection.test.ts
+++
@@ -1,9 +0,0 @@
-import {MongoConnection} from "./MongoConnection"
-
-test('Deve conectar a um cliente mongodb', async () => {
-    // const client = new MongoConnection({
-    //     database: 'america',
-    // });
-    // const connectionResponse = await client.connect();
-    // expect(connectionResponse).toBeTruthy();
-})
