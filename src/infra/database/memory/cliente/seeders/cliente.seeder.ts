321b698f11e3d774e768a5bfb263fdc1ae550497
andreappmarketing <andre@appmarketing.com.br>
2024-01-06 11:26:02 -0300

    fix(seeder): remove seeders de cliente e item

--- src/infra/database/memory/cliente/seeders/cliente.seeder.ts
+++ src/infra/database/memory/cliente/seeders/cliente.seeder.ts
@@ -2,15 +2,12 @@ import { DataReader } from "@shared/ports/dataReader";
 import { Repository } from "@shared/ports/repository";
 import { Seeder } from "@shared/ports/seeder";
 import { Cliente, ClienteProps } from "@domain/cliente/entities/cliente";
-import { CadastrarClienteUseCase } from "@domain/cliente/usecases/cadastrarCliente.usecase";
 
 export class ClienteSeeder implements Seeder {
-  private cadastrarClienteUseCase: CadastrarClienteUseCase;
   constructor(
     private readonly repository: Repository<Cliente>,
     private readonly dataReader: DataReader<ClienteProps[]>
   ) {
-    this.cadastrarClienteUseCase = new CadastrarClienteUseCase(repository);
   }
 
   async seed(): Promise<number> {
@@ -21,7 +18,6 @@ export class ClienteSeeder implements Seeder {
       const expectedLength = data.length;
       await Promise.all(
         data.map(async (cliente) => {
-          return this.cadastrarClienteUseCase.execute(cliente);
         })
       );
 
